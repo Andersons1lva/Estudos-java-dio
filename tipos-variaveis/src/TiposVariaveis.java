1f8b3c44f17d6ebe66745dc5e02e5b35b94007b5
andersons1lva <andersons1lva@yahoo.com.br>
2022-11-01 22:47:20 -0300

    variavel final

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // para declarar uma variavél e garantir que essa não seja alterada o seu valor, usamos "final e seu nome em CAIXA ALTA".
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
