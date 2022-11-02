9d88b78bd927848c66d04c99d9d69e2020c54746
andersons1lva <andersons1lva@yahoo.com.br>
2022-11-02 16:26:07 -0300

    tipos variados de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,69 @@
+public class Operadores {
+    public static void main(String[] args) {
+       /*  // Sinal de + entre os 2 nomes e chamado de concatenação (Serve para Juntar as 2 palavras para tornar uma só)
+
+       String nomeCompleto = "LINGUAGEM " + "JAVA";
+       System.out.println(nomeCompleto);
+
+       // OBS:Só quando os numeros vem primeiro ocorre a soma e depois a 
+       concatenação(em quando que outra posição tiver uma string o mesmo só concateniza).
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+            System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+            System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+            System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+            System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);*/
+
+
+        /* 
+        // para deixar um numero negativo é preciso: numero = -numero; ou seja ele vai receber ele mesmo negativo.
+        int numero = 5;
+
+        numero = -numero;
+        
+        System.out.println(numero);
+        // para tornar um valor de fomar positiva preciso multiplicar por -1. Ex: numero = numero * -1
+        numero = numero * -1;
+        System.out.println(numero);*/
+
+        /*int a, b;
+        a = 2;
+        b = 2;
+        
+        //If Ternario você chama ele direto na variavel.
+        String resultado = a==b ? "verdadeiro" : "false";        
+        System.out.println(resultado);*/
+
+
+        /*int numero1 = 4;
+        int numero2 = 4;
+        boolean simNao = numero1 == numero2;
+        System.out.println("NúmeroUm é igual ao NúmeroDois ? :" + simNao);
+
+       // if(numero1 == numero2){
+      //      System.out.println("a nossa expressão é verdadeira");
+      //  }
+
+        boolean feriado = true;
+        boolean sol = false;
+        String vamosAPraia;
+
+        vamosAPraia = feriado && sol ? "Então vamos a Praia" : "Vamos Ficar em Casa";
+        System.out.println(vamosAPraia);
+
+        boolean chovendoMuito = false;
+        boolean chovendo = false;
+        String levarGuardaChuva;
+
+        levarGuardaChuva = chovendo || chovendoMuito ? "Leve o Guarda-chuva" : "Não levar o Guarda-chuva";
+        System.out.println(levarGuardaChuva);*/
+
+        
+    }
+}
