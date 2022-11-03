3459a3f62cffabb7015ff1c23361d578d7f6c7cd
andersons1lva <andersons1lva@yahoo.com.br>
2022-11-03 16:13:04 -0300

    criando atributos e objetos da classe SmartTv

---
+++ tipos-variaveis/sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,30 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+        System.out.println("Televisão Ligada" + ligada);
+    }
+
+    public void desligada(){
+        ligada = false;
+        System.out.println("Televisão Desligada" + ligada);
+    }
+
+    public void aumentarVolume(){
+        volume ++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume --;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void mudarCanal(){
+        canal++;
+        System.out.println("Mudando o Canal para: " + canal);
+    }
+}
